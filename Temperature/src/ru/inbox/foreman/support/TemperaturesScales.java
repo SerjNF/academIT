b606e2596ffc093d198a8f0313590adcda9cbed4
SergeyNF <foreman@inbox.ru>
2018-08-30 20:53:19 +0700

    temperature fix interface

--- Temperature/src/ru/inbox/foreman/support/TemperaturesScales.java
+++ Temperature/src/ru/inbox/foreman/support/TemperaturesScales.java
@@ -2,10 +2,10 @@ package ru.inbox.foreman.support;
 
 public interface TemperaturesScales {
 
-    public double convertToCelsius(double temperature);
+    double convertToCelsius(double temperature);
 
-    public double convertFromCelsius(double temperature);
+    double convertFromCelsius(double temperature);
 
-    public String getScaleName();
+    String getScaleName();
 
 }
