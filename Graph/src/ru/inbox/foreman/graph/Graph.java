9f96fb1c5ed22e133a2813bba62f6c0fa5a0805c
SerjNF <foreman@inbox.ru>
2018-09-06 12:54:20 +0700

    graph update

--- Graph/src/ru/inbox/foreman/graph/Graph.java
+++ Graph/src/ru/inbox/foreman/graph/Graph.java
@@ -3,7 +3,7 @@ package ru.inbox.foreman.graph;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Stack;
-import java.util.function.Consumer;
+import java.util.function.IntConsumer;
 
 public class Graph {
     private int[][] nodes;
@@ -12,7 +12,7 @@ public class Graph {
         this.nodes = nodes;
     }
 
-    public void visitDepthFirst(Consumer<Integer> consumer) {
+    public void visitDepthFirst(IntConsumer consumer) {
         boolean[] visitedNodes = new boolean[nodes.length];
         Stack<Integer> stack = new Stack<>();
 
@@ -25,11 +25,12 @@ public class Graph {
                     if (!visitedNodes[currentNode]) {
                         consumer.accept(currentNode);
                         visitedNodes[currentNode] = true;
-                    }
-                    int[] currentNodeLinks = nodes[currentNode];
-                    for (int j = currentNodeLinks.length - 1; j >= 0; j--) {
-                        if (currentNodeLinks[j] == 1 && !visitedNodes[j]) {
-                            stack.add(j);
+
+                        int[] currentNodeLinks = nodes[currentNode];
+                        for (int j = currentNodeLinks.length - 1; j >= 0; j--) {
+                            if (currentNodeLinks[j] == 1 && !visitedNodes[j]) {
+                                stack.add(j);
+                            }
                         }
                     }
                 }
@@ -37,7 +38,7 @@ public class Graph {
         }
     }
 
-    public void visitBreadthFirst(Consumer<Integer> consumer) {
+    public void visitBreadthFirst(IntConsumer consumer) {
         boolean[] visitedNodes = new boolean[nodes.length];
         Queue<Integer> queue = new ArrayDeque<>();
 
@@ -50,11 +51,12 @@ public class Graph {
                     if (!visitedNodes[currentNode]) {
                         consumer.accept(currentNode);
                         visitedNodes[currentNode] = true;
-                    }
-                    int[] currentNodeLinks = nodes[currentNode];
-                    for (int j = 0; j < currentNodeLinks.length; j++) {
-                        if (currentNodeLinks[j] == 1 && !visitedNodes[j]) {
-                            queue.add(j);
+
+                        int[] currentNodeLinks = nodes[currentNode];
+                        for (int j = 0; j < currentNodeLinks.length; j++) {
+                            if (currentNodeLinks[j] == 1 && !visitedNodes[j]) {
+                                queue.add(j);
+                            }
                         }
                     }
                 }
@@ -62,8 +64,9 @@ public class Graph {
         }
     }
 
-    public void visitDepthFirstRecurs(Consumer<Integer> consumer) {
+    public void visitDepthFirstRecurs(IntConsumer consumer) {
         boolean[] visitedNodes = new boolean[nodes.length];
+
         for (int k = 0; k < nodes.length; k++) {
             if (!visitedNodes[k]) {
                 visitRecurs(consumer, k, visitedNodes);
@@ -71,10 +74,11 @@ public class Graph {
         }
     }
 
-    private void visitRecurs(Consumer<Integer> consumer, int node, boolean[] visitedNodes) {
+    private void visitRecurs(IntConsumer consumer, int node, boolean[] visitedNodes) {
         consumer.accept(node);
         visitedNodes[node] = true;
         int[] array = nodes[node];
+
         for (int i = 0; i < array.length; i++) {
             if (nodes[node][i] == 1 && !visitedNodes[i]) {
                 visitRecurs(consumer, i, visitedNodes);
